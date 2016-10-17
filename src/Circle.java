a17a63f9b7c2a2827dd775812d8ed65e8ad576df
FuelDoks <fueldoks@gmail.com>
2016-10-18 02:59:16 +0300

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,14 @@
+public class Circle {
+    public float x;
+    public float y;
+    public float radius;
+
+    public static void main() {
+    Circle circleA = new Circle();
+        circleA.x = 124;
+        circleA.y = 234;
+        circleA.radius = 666;
+        System.out.println("CircleA.x"+"CircleA.y"+"CircleA.radius");
+
+    }
+}
