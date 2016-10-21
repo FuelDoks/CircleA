54853ba261a6a57f4ae7e7cb1b9d0dc6fde97916
FuelDoks <fueldoks@gmail.com>
2016-10-21 18:16:47 +0300

    circle

---
+++ src/CircleA/NewCircles.java
@@ -0,0 +1,125 @@
+public class NewCircles {
+    float x;
+    float y;
+    float radius;
+
+    class CircleF {
+        public void main(String[] args) {
+            Circle circleF = new Circle();
+            Circle circleD = new Circle();
+            Circle circleE = new Circle();
+            Circle circleA = new Circle();
+            Circle circleV = new Circle();
+            Circle circleB = new Circle();
+            Circle circleZ = new Circle();
+            Circle circleQ = new Circle();
+            Circle circleL = new Circle();
+            Circle circleK = new Circle();
+            Circle circleN = new Circle();
+            Circle circleG = new Circle();
+            Circle circleS = new Circle();
+            Circle circleT = new Circle();
+            Circle circleJ = new Circle();
+            Circle circleP = new Circle();
+            Circle circleO = new Circle();
+            Circle circleI = new Circle();
+
+            circleA.x = 12;
+            circleA.y = 11;
+            circleA.radius = 9;
+
+            circleB.x = 15;
+            circleB.y = 16;
+            circleB.radius = 11;
+
+            circleD.x = 15;
+            circleD.y = 9:
+            circleD.radius =4;
+
+            circleE.x = 21;
+            circleE.y = 20;
+            circleE.radius = 11;
+
+            circleF.x = 18;
+            circleF.y = 16;
+            circleF.radius = 10;
+
+            circleG.x = 10;
+            circleG.y = 9;
+            circleG.radius = 5;
+
+            circleI.x = 16;
+            circleI.y = 14;
+            circleI.radius = 8;
+
+            circleJ.x = 32;
+            circleJ.y = 24;
+            circleJ.radius = 11;
+
+            circleK.x = 24;
+            circleK.y = 12;
+            circleK.radius = 8;
+
+            circleL.x = 27;
+            circleL.y = 18;
+            circleL.radius = 7;
+
+            circleN.x = 38;
+            circleN.y = 27;
+            circleN.radius = 13;
+
+            circleO.x = 25;
+            circleO.y = 15;
+            circleO.radius = 5;
+
+            circleP.x = 26;
+            circleP.x = 16;
+            circleP.radius = 6;
+
+            circleQ.x = 27;
+            circleQ.y = 17;
+            circleQ.radius = 7;
+
+            circleS.x = 28;
+            circleS.y = 18;
+            circleS.radius = 8;
+
+            circleT.x = 29;
+            circleT.y = 19;
+            circleT.radius = 9;
+
+            circleV.x = 30;
+            circleV.y = 20;
+            circleV.radius = 10;
+
+            circleZ.x = 31;
+            circleZ.y = 21;
+            circleZ.radius = 11;
+
+
+
+
+            System.out.println("circleA" + circleA.x + "" + circleA.y + "" + circleA.radius);
+            System.out.println("circleB" + circleB.x + "" + circleB.y + "" + circleB.radius);
+            System.out.println("circleD" + circleD.x + "" + circleD.y + "" + circleD.radius);
+            System.out.println("circleE" + circleE.x + "" + circleE.y + "" + circleE.radius);
+            System.out.println("circleF" + circleF.x + "" + circleF.y + "" + circleF.radius);
+            System.out.println("circleG" + circleG.x + "" + circleG.y + "" + circleG.radius);
+            System.out.println("circleI" + circleI.x + "" + circleI.y + "" + circleI.radius);
+            System.out.println("circleJ" + circleJ.x + "" + circleJ.y + "" + circleJ.radius);
+            System.out.println("circleK" + circleK.x + "" + circleK.y + "" + circleK.radius);
+            System.out.println("circleL" + circleL.x + "" + circleL.y + "" + circleL.radius);
+            System.out.println("circleN" + circleN.x + "" + circleN.y + "" + circleN.radius);
+            System.out.println("circleO" + circleO.x + "" + circleO.y + "" + circleO.radius);
+            System.out.println("circleP" + circleP.x + "" + circleP.y + "" + circleP.radius);
+            System.out.println("circleQ" + circleQ.x + "" + circleQ.y + "" + circleQ.radius);
+            System.out.println("circleS" + circleS.x + "" + circleS.y + "" + circleS.radius);
+            System.out.println("circleT" + circleT.x + "" + circleT.y + "" + circleT.radius);
+            System.out.println("circleV" + circleV.x + "" + circleV.y + "" + circleV.radius);
+            System.out.println("circleZ" + circleZ.x + "" + circleZ.y + "" + circleZ.radius);
+
+
+        }
+
+    }
+}
